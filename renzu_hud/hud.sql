142cde99f227da5681a131f45a83c812647cfd51
renzuzu <82306584+renzuzu@users.noreply.github.com>
2021-09-18 11:41:05 +0800

    Update hud.sql

--- renzu_hud/hud.sql
+++ renzu_hud/hud.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `vehicle_status` (
 	`stats` LONGTEXT NULL COLLATE 'utf8mb4_general_ci',
-	`plate` VARCHAR(64) NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
-	`owner` VARCHAR(64) NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	`plate` VARCHAR(64) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	`owner` VARCHAR(64) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
 	PRIMARY KEY (`plate`) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
@@ -9,7 +9,7 @@ ENGINE=InnoDB
 ;
 CREATE TABLE IF NOT EXISTS `body_status` (
 	`status` LONGTEXT NULL COLLATE 'utf8mb4_general_ci',
-	`identifier` VARCHAR(64) NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	`identifier` VARCHAR(64) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
 	PRIMARY KEY (`identifier`) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
