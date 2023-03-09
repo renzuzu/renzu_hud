eaede9377760d0ead66cecff512282319ff56011
renzuzu <82306584+renzuzu@users.noreply.github.com>
2023-03-10 01:38:54 +0800

    tweak(conf/status): reformat

--- conf/status.lua
+++ conf/status.lua
@@ -1,4 +1,4 @@
---STATUS CONFIG
+-- STATUS CONFIG
 config.statusv1sleep = 1000 -- 1 second update
 config.registerautostatus = true -- register all status from config.statusordering setting below to ESX STATUS or RENZU_STATUS (only the status with custom bool will be registered example: line 60:status.lua) (ESX STATUS ONLY, RENZU_STATUS ALREADY HAVE A AUTO REGISTER FUNC)
 config.statusui = 'simple' -- UI LOOK ( simple, normal ) -- NORMAL = with pedface, Simple = Only Icons
@@ -7,9 +7,9 @@ config.statusv2 = true -- enable this if you want the status toggle mode (TOGGLE
 config.statusplace = 'bottom-left' -- available option top-right,top-left,bottom-right,bottom-left,top-center,bottom-center
 config.uidesign = 'circle' -- octagon (default), circle, square
 config.QbcoreStatusDefault = false -- if true will use metadatas as default you can stop renzu_status
---CHANGE ACCORDING TO YOUR STATUS ESX STATUS OR ANY STATUS MOD
+-- CHANGE ACCORDING TO YOUR STATUS ESX STATUS OR ANY STATUS MOD
 
---STATUS ( disabled v2 if you want the optimize version ( FETCH ONLY The Player Status if Toggled ) else v2 is running loop default of 1sec)
+-- STATUS ( disabled v2 if you want the optimize version ( FETCH ONLY The Player Status if Toggled ) else v2 is running loop default of 1sec)
 config.statusv2_sleep = 1000 -- 1sec
 config.statusnotify = true
 config.driving_affect_status = true -- should the status will be affected during Driving a vehicle?
@@ -38,22 +38,48 @@ config.parachute_affect_status = true -- while parachuting mode can add status?
 config.parachute_affected_status = 'stress' -- type of status
 config.parachute_status_mode = 'remove' -- status remove or add?
 config.parachute_status_val = 10000 -- value to add/remove
---status standalone purpose (use this only if you need it)
+-- status standalone purpose (use this only if you need it)
 -- Add / Remove Status when playing some animations
 config.playing_animation_affect_status = true
 config.status_animation = {
-	--this is a DICT
-	{ dict = 'mp_player_inteat@burger', name='mp_player_int_eat_burger', status = 'hunger', mode = 'add', val = '300000'},
-	{ dict = 'mp_player_inteat@burger', name='mp_player_int_eat_burger_fp', status = 'hunger', mode = 'add', val = '300000'},
-	{ dict = 'mp_player_intdrink', name='loop_bottle', status = 'thirst', mode = 'add', val = '300000'},
-	{ dict = 'mp_player_intdrink', name='loop_bottle_fp', status = 'thirst', mode = 'add', val = '300000'},
-	{ dict = 'amb@world_human_aa_smoke@male@idle_a', name='idle_c', status = 'sanity', mode = 'remove', val = '10000'},
+    -- this is a DICT
+    {
+        dict = 'mp_player_inteat@burger',
+        name = 'mp_player_int_eat_burger',
+        status = 'hunger',
+        mode = 'add',
+        val = '300000'
+    }, {
+        dict = 'mp_player_inteat@burger',
+        name = 'mp_player_int_eat_burger_fp',
+        status = 'hunger',
+        mode = 'add',
+        val = '300000'
+    }, {
+        dict = 'mp_player_intdrink',
+        name = 'loop_bottle',
+        status = 'thirst',
+        mode = 'add',
+        val = '300000'
+    }, {
+        dict = 'mp_player_intdrink',
+        name = 'loop_bottle_fp',
+        status = 'thirst',
+        mode = 'add',
+        val = '300000'
+    }, {
+        dict = 'amb@world_human_aa_smoke@male@idle_a',
+        name = 'idle_c',
+        status = 'sanity',
+        mode = 'remove',
+        val = '10000'
+    }
 
-	-- you can add as many animation as you want
+    -- you can add as many animation as you want
 }
 
---if you want to add more status and reorder it.
---the config have the div id's, offsets, colors, classes per status.
+-- if you want to add more status and reorder it.
+-- the config have the div id's, offsets, colors, classes per status.
 -- if config.registerautostatus is enable:
 -- startvalue is important and default to 1000000, and remove value per tick is default for 100
 
@@ -69,79 +95,236 @@ config.status_animation = {
 -- value == leave it default 0
 -- status = the status name
 -- OTHER OPTION ARE ICON CUSTOMIZATION for Colors, Fontawsome etc..
-config.statusordering = { -- SET enable = false to disable the status (the status must be registered from your esx_status) i highly suggest to use my standalone_status (https://github.com/renzuzu/renzu_status) so you wont have to edit the special events and exports needed for the status System!
-	[0] = {type = 1, enable = true, status = 'health', hideifmax = false, custom = false, value = 100, notify_lessthan = false, notify_message = 'i am very hungry', notify_value = 20, display = 'none', offset = '275', i_id_1_color = 'rgb(101, 255, 131)',bg = 'rgb(20,23,26)', fa = 'fas fa-heartbeat', i_id_2_color = 'rgba(251, 29, 9, 0.3)'},
-	[1] = {type = 1, enable = true, status = 'armor', hideifmax = true, custom = false, value = 0, notify_lessthan = false, notify_message = 'i am very hungry', notify_value = 20, display = 'none', offset = '275', i_id_1_color = 'rgb(1, 103, 255)', bg = 'rgb(20,23,26)',fa = 'far fa-shield-alt', i_id_2_color = 'rgb(0, 41, 129)'},
-	[2] = {type = 1, enable = true, status = 'hunger', hideifmax = false, custom = true, value = 0, startvalue = 1000000, statusremove = 100, notify_lessthan = false, notify_message = 'i am very hungry', notify_value = 20, display = 'block', offset = '275', i_id_1_color = 'rgb(255, 155, 39)',bg = 'rgb(20,23,26)', fa = 'fas fa-cheeseburger', i_id_2_color = 'rgb(114, 68, 0)'},
-	[3] = {type = 1, enable = true, status = 'thirst', hideifmax = false, custom = true, value = 0, startvalue = 1000000, statusremove = 100, notify_lessthan = false, notify_message = 'i am very thirsty', notify_value = 20, display = 'block', offset = '275', i_id_1_color = 'rgb(36, 113, 255)',bg = 'rgb(20,23,26)', fa = 'fad fa-glass', i_id_2_color = 'rgb(0, 11, 112)'},
-	[4] = {type = 1, enable = false, status = 'stress', hideifmax = false, min_val_hide = 50, custom = true, value = 0, startvalue = 0, statusremove = 0, notify_lessthan = true, notify_message = 'i see some dragons', notify_value = 80, display = 'block', offset = '275', i_id_1_color = 'rgb(255, 16, 68)',bg = 'rgb(20,23,26)', fa = 'fad fa-head-side-brain', i_id_2_color = 'rgba(35, 255, 101, 0.84)'},
-	[5] = {type = 1, enable = true, status = 'stamina', hideifmax = true, min_val_hide = 100, custom = false, value = 0, notify_lessthan = false, notify_message = 'running makes me thirsty', notify_value = 20, display = 'block', offset = '275', i_id_1_color = 'rgb(16, 255, 136)',bg = 'rgb(20,23,26)', fa = 'fad fa-running', i_id_2_color = 'rgba(0, 119, 57, 0.94)'},
-	[6] = {type = 1, enable = true, status = 'oxygen', hideifmax = true, min_val_hide = 100, custom = false, value = 0, notify_lessthan = false, notify_message = 'my oxygen is almost gone', notify_value = 20, display = 'block', offset = '275', i_id_1_color = 'rgb(15, 227, 255)',bg = 'rgb(20,23,26)', fa = 'fad fa-lungs', i_id_2_color = 'rgba(8, 76, 85, 0.78)'},
-	[7] = {type = 1, enable = true, status = 'energy', hideifmax = false, min_val_hide = 50, custom = true, value = 0, startvalue = 1000000, statusremove = 100, notify_lessthan = false, notify_message = 'i am very tired', notify_value = 20, display = 'block', offset = '275', i_id_1_color = 'rgb(233, 233, 233)',bg = 'rgb(20,23,26)', fa = 'fas fa-snooze', i_id_2_color = 'color:rgb(243, 57, 0)'},
-	[8] = {type = 1, enable = true, status = 'voip', hideifmax = false, custom = false, value = 0, notify_lessthan = false, notify_message = 'silent mode', notify_value = 0, display = 'block', offset = '275', i_id_1_color = 'rgb(255, 255, 255)',bg = 'rgb(20,23,26)', fa = 'fas fa-microphone', i_id_2_color = 'rgba(251, 29, 9, 0.3)'},
-	
-	--SAMPLE ONLY
-	[9] = {
-		type = 1, -- 1 = circle progress or 0 = flat progress bar
-		enable = false,  -- enable or disable status
-		status = 'poop',  -- status name from esx_status or renzu_status
-		hideifmax = false,  -- hide the status if its max (stress and armor is inverted)
-		min_val_hide = 50, -- works only if hideifmax == true (show/hide the status if value is met)
-		custom = true, -- native (ex. oxygen) or custom (Esx status/renzu_status)
-		value = 0, -- leave default 0
-		startvalue = 1000000,
-		statusremove = 100, -- this will be register to esx_status or renzu_status, ( this is the remove value per tick )
-		notify_lessthan = false, -- inverted ?
-		notify_message = 'my poop is cumming',  -- notify message
-		notify_value = 25,  -- notify percentage trigger
-		display = 'block',  -- dont change
-		offset = '275',  -- default offset
-		fa = 'fas fa-poo',  -- font awsome
-		i_id_1_color = 'rgb(255, 199, 102)', -- progress color
-		i_id_2_color = 'rgba(251, 29, 9, 0.3)', -- use if icons type
-		bg = 'rgb(20,23,26)',
-	},
-	[10] = {
-		type = 1, 
-		enable = false, 
-		status = 'pee', 
-		hideifmax = false,
-		min_val_hide = 50,
-		custom = true, 
-		value = 0, 
-		startvalue = 1000000,
-		statusremove = 100, -- this will be register to esx_status or renzu_status, ( this is the remove value per tick )
-		notify_lessthan = false, 
-		notify_message = 'my pee is cumming', 
-		notify_value = 25, 
-		display = 'block', 
-		offset = '275', 
-		fa = 'fas fa-restroom', -- font awsome
-		i_id_1_color = 'rgb(255, 245, 112)', -- progress color
-		i_id_2_color = 'rgba(251, 29, 9, 0.3)', -- bg color
-	bg = 'rgb(20,23,26)',
-	},
-	[11] = {
-		type = 1, 
-		enable = false, 
-		status = 'hygiene', 
-		hideifmax = false,
-		min_val_hide = 50,
-		custom = true, 
-		value = 0, 
-		startvalue = 1000000,
-		statusremove = 100, -- this will be register to esx_status or renzu_status, ( this is the remove value per tick )
-		notify_lessthan = false, 
-		notify_message = 'i smell fucking bad', 
-		notify_value = 25, 
-		display = 'block', 
-		offset = '275', 
-		fa = 'fad fa-shower', -- font awsome
-		i_id_1_color = 'rgb(102, 196, 255)', -- progress color
-		i_id_2_color = 'rgba(251, 29, 9, 0.3)', -- bg color
-	bg = 'rgb(20,23,26)',
-	},
-}
+config.statusordering =
+    { -- SET enable = false to disable the status (the status must be registered from your esx_status) i highly suggest to use my standalone_status (https://github.com/renzuzu/renzu_status) so you wont have to edit the special events and exports needed for the status System!
+        [0] = {
+            type = 1,
+            enable = true,
+            status = 'health',
+            hideifmax = false,
+            custom = false,
+            value = 100,
+            notify_lessthan = false,
+            notify_message = 'i am very hungry',
+            notify_value = 20,
+            display = 'none',
+            offset = '275',
+            i_id_1_color = 'rgb(101, 255, 131)',
+            bg = 'rgb(20,23,26)',
+            fa = 'fas fa-heartbeat',
+            i_id_2_color = 'rgba(251, 29, 9, 0.3)'
+        },
+        [1] = {
+            type = 1,
+            enable = true,
+            status = 'armor',
+            hideifmax = true,
+            custom = false,
+            value = 0,
+            notify_lessthan = false,
+            notify_message = 'i am very hungry',
+            notify_value = 20,
+            display = 'none',
+            offset = '275',
+            i_id_1_color = 'rgb(1, 103, 255)',
+            bg = 'rgb(20,23,26)',
+            fa = 'far fa-shield-alt',
+            i_id_2_color = 'rgb(0, 41, 129)'
+        },
+        [2] = {
+            type = 1,
+            enable = true,
+            status = 'hunger',
+            hideifmax = false,
+            custom = true,
+            value = 0,
+            startvalue = 1000000,
+            statusremove = 100,
+            notify_lessthan = false,
+            notify_message = 'i am very hungry',
+            notify_value = 20,
+            display = 'block',
+            offset = '275',
+            i_id_1_color = 'rgb(255, 155, 39)',
+            bg = 'rgb(20,23,26)',
+            fa = 'fas fa-cheeseburger',
+            i_id_2_color = 'rgb(114, 68, 0)'
+        },
+        [3] = {
+            type = 1,
+            enable = true,
+            status = 'thirst',
+            hideifmax = false,
+            custom = true,
+            value = 0,
+            startvalue = 1000000,
+            statusremove = 100,
+            notify_lessthan = false,
+            notify_message = 'i am very thirsty',
+            notify_value = 20,
+            display = 'block',
+            offset = '275',
+            i_id_1_color = 'rgb(36, 113, 255)',
+            bg = 'rgb(20,23,26)',
+            fa = 'fad fa-glass',
+            i_id_2_color = 'rgb(0, 11, 112)'
+        },
+        [4] = {
+            type = 1,
+            enable = false,
+            status = 'stress',
+            hideifmax = false,
+            min_val_hide = 50,
+            custom = true,
+            value = 0,
+            startvalue = 0,
+            statusremove = 0,
+            notify_lessthan = true,
+            notify_message = 'i see some dragons',
+            notify_value = 80,
+            display = 'block',
+            offset = '275',
+            i_id_1_color = 'rgb(255, 16, 68)',
+            bg = 'rgb(20,23,26)',
+            fa = 'fad fa-head-side-brain',
+            i_id_2_color = 'rgba(35, 255, 101, 0.84)'
+        },
+        [5] = {
+            type = 1,
+            enable = true,
+            status = 'stamina',
+            hideifmax = true,
+            min_val_hide = 100,
+            custom = false,
+            value = 0,
+            notify_lessthan = false,
+            notify_message = 'running makes me thirsty',
+            notify_value = 20,
+            display = 'block',
+            offset = '275',
+            i_id_1_color = 'rgb(16, 255, 136)',
+            bg = 'rgb(20,23,26)',
+            fa = 'fad fa-running',
+            i_id_2_color = 'rgba(0, 119, 57, 0.94)'
+        },
+        [6] = {
+            type = 1,
+            enable = true,
+            status = 'oxygen',
+            hideifmax = true,
+            min_val_hide = 100,
+            custom = false,
+            value = 0,
+            notify_lessthan = false,
+            notify_message = 'my oxygen is almost gone',
+            notify_value = 20,
+            display = 'block',
+            offset = '275',
+            i_id_1_color = 'rgb(15, 227, 255)',
+            bg = 'rgb(20,23,26)',
+            fa = 'fad fa-lungs',
+            i_id_2_color = 'rgba(8, 76, 85, 0.78)'
+        },
+        [7] = {
+            type = 1,
+            enable = true,
+            status = 'energy',
+            hideifmax = false,
+            min_val_hide = 50,
+            custom = true,
+            value = 0,
+            startvalue = 1000000,
+            statusremove = 100,
+            notify_lessthan = false,
+            notify_message = 'i am very tired',
+            notify_value = 20,
+            display = 'block',
+            offset = '275',
+            i_id_1_color = 'rgb(233, 233, 233)',
+            bg = 'rgb(20,23,26)',
+            fa = 'fas fa-snooze',
+            i_id_2_color = 'color:rgb(243, 57, 0)'
+        },
+        [8] = {
+            type = 1,
+            enable = true,
+            status = 'voip',
+            hideifmax = false,
+            custom = false,
+            value = 0,
+            notify_lessthan = false,
+            notify_message = 'silent mode',
+            notify_value = 0,
+            display = 'block',
+            offset = '275',
+            i_id_1_color = 'rgb(255, 255, 255)',
+            bg = 'rgb(20,23,26)',
+            fa = 'fas fa-microphone',
+            i_id_2_color = 'rgba(251, 29, 9, 0.3)'
+        },
+
+        -- SAMPLE ONLY
+        [9] = {
+            type = 1, -- 1 = circle progress or 0 = flat progress bar
+            enable = false, -- enable or disable status
+            status = 'poop', -- status name from esx_status or renzu_status
+            hideifmax = false, -- hide the status if its max (stress and armor is inverted)
+            min_val_hide = 50, -- works only if hideifmax == true (show/hide the status if value is met)
+            custom = true, -- native (ex. oxygen) or custom (Esx status/renzu_status)
+            value = 0, -- leave default 0
+            startvalue = 1000000,
+            statusremove = 100, -- this will be register to esx_status or renzu_status, ( this is the remove value per tick )
+            notify_lessthan = false, -- inverted ?
+            notify_message = 'my poop is cumming', -- notify message
+            notify_value = 25, -- notify percentage trigger
+            display = 'block', -- dont change
+            offset = '275', -- default offset
+            fa = 'fas fa-poo', -- font awsome
+            i_id_1_color = 'rgb(255, 199, 102)', -- progress color
+            i_id_2_color = 'rgba(251, 29, 9, 0.3)', -- use if icons type
+            bg = 'rgb(20,23,26)'
+        },
+        [10] = {
+            type = 1,
+            enable = false,
+            status = 'pee',
+            hideifmax = false,
+            min_val_hide = 50,
+            custom = true,
+            value = 0,
+            startvalue = 1000000,
+            statusremove = 100, -- this will be register to esx_status or renzu_status, ( this is the remove value per tick )
+            notify_lessthan = false,
+            notify_message = 'my pee is cumming',
+            notify_value = 25,
+            display = 'block',
+            offset = '275',
+            fa = 'fas fa-restroom', -- font awsome
+            i_id_1_color = 'rgb(255, 245, 112)', -- progress color
+            i_id_2_color = 'rgba(251, 29, 9, 0.3)', -- bg color
+            bg = 'rgb(20,23,26)'
+        },
+        [11] = {
+            type = 1,
+            enable = false,
+            status = 'hygiene',
+            hideifmax = false,
+            min_val_hide = 50,
+            custom = true,
+            value = 0,
+            startvalue = 1000000,
+            statusremove = 100, -- this will be register to esx_status or renzu_status, ( this is the remove value per tick )
+            notify_lessthan = false,
+            notify_message = 'i smell fucking bad',
+            notify_value = 25,
+            display = 'block',
+            offset = '275',
+            fa = 'fad fa-shower', -- font awsome
+            i_id_1_color = 'rgb(102, 196, 255)', -- progress color
+            i_id_2_color = 'rgba(251, 29, 9, 0.3)', -- bg color
+            bg = 'rgb(20,23,26)'
+        }
+    }
 
 -- RegisterCommand('statusadd', function()
 -- 	TriggerEvent('esx_status:add', 'stress', 520000)
